530e89817756c6cbd4d2d8d6ce8fe146e82c5cad
Kartik M.P <Kartik.pulipati@gmail.com>
2022-03-08 20:27:07 -0800

    Tried to make it work

--- getEmotion.py
+++ getEmotion.py
@@ -8,19 +8,34 @@ eye_cascade = cv2.CascadeClassifier(
 mouth_cascade = cv2.CascadeClassifier(
                 '/Users/kartik/opt/anaconda3/envs/pytorchenv/share/OpenCV/haarcascades/haarcascade_mcs_mouth.xml')
 
-frame0 = cv2.VideoCapture(0)
-frame1 = cv2.VideoCapture(2)
+
+def vConcat_Resize(img_list, interpolation=cv2.INTER_CUBIC):
+    # take minimum width
+    w_min = min(img.shape[1]
+                for img in img_list)
+
+    # resizing images
+    im_list_resize = [cv2.resize(img,
+                                 (w_min, int(img.shape[0] * w_min / img.shape[1])),
+                                 interpolation=interpolation)
+                      for img in img_list]
+    # return final image
+    return cv2.vconcat(im_list_resize)
+#
+frame0 = cv2.VideoCapture(1)
+# frame1 = cv2.VideoCapture(2)
+
 
 while 1:
     ret0, img1 = frame0.read()
-    ret1, img2 = frame1.read()
-    img11 = cv2.resize(img1, (256, 256))
-    img22 = cv2.resize(img2, (256, 256))
+    # ret1, img2 = frame1.read()
+    img11 = cv2.resize(img1, (500, 500))
+    # img22 = cv2.resize(img2, (256, 256))
     ax = []
     ay = []
     ax2 = []
     ay2 = []
-    mouths = 0
+    mouths = ()
 
     if frame0:
         gray = cv2.cvtColor(img11, cv2.COLOR_BGR2GRAY)
@@ -33,22 +48,24 @@ while 1:
             ay2.append(ey+eh)
 
         cv2.imshow('Eye', img11)
-    if frame1:
-        gray = cv2.cvtColor(img22, cv2.COLOR_BGR2GRAY)
-        mouth = mouth_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=70)
-        for (mx, my, mw, mh) in mouth:
-            img22 = cv2.rectangle(img22, (mx, my), (mx + mw, my + mh), (0, 256, 0), 1)
-            mouths = (mx, my, mx + mw, my + mh)
-        cv2.imshow('Mouth', img22)
+    # if frame1:
+    #     gray = cv2.cvtColor(img22, cv2.COLOR_BGR2GRAY)
+    #     mouth = mouth_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=70)
+    #     for (mx, my, mw, mh) in mouth:
+    #         img22 = cv2.rectangle(img22, (mx, my), (mx + mw, my + mh), (0, 255, 0), 1)
+    #         mouths = (mx, my, mx + mw, my + mh)
+    #     cv2.imshow('Mouth', img22)
 
-        if ax and ay and ax2 and ay2 and mouth != 0:
-            result = np.concatenate(img11[min(ax):max(ax2), min(ay):max(ay2)], img22[mouths[0]:mouths[2], mouths[1]:mouths[2]])
-            cv2.imshow('Result', result)
+    # if len(mouths) != 0 and len(ay2) == 2:
+    #     eye = img11[min(ay)-10:max(ay2)+10, min(ax)-10:max(ax2)+10]
+    #     mouth = img22[mouths[1]-10:mouths[3]+10, mouths[0]-10:mouths[2]]
+    #     result = vConcat_Resize([eye, mouth])
+    #     cv2.imshow('Result', result)
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
-      break
+        break
 
 frame0.release()
-frame1.release()
+# frame1.release()
 cv2.destroyAllWindows()
